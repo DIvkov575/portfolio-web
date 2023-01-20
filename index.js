4b9fd8bb52dcd5b289cbf2c9ed8d916fa311897d
DIvkov575 <Dmitriy@Ivkov.net>
2023-01-20 17:47:35 -0500

    moving to ejs

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 app.use(express.static('src'));
+app.set('view engine', 'ejs');
 // app.use(express.json())
 // ---------------------------------
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/src/home.html');
+    // res.sendFile(__dirname + '/src/home.html')
+    res.render('home', { input: [{ a: 'title', b: 'contetn', imgPath: 'test' }] });
 });
 app.listen(3000, () => {
     console.log("App listening on port 3000");
--- index.ts
+++ index.ts
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('src'))
+app.set('view engine', 'ejs')
 // app.use(express.json())
 
 // ---------------------------------
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/src/home.html')
+  // res.sendFile(__dirname + '/src/home.html')
+  res.render('home', {input:[{a:'title', b:'contetn', imgPath: 'test'}]})
 })
 
 
